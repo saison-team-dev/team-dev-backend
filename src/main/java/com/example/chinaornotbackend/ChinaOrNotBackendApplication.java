45e2c3e1f56a417e66bd3688c319a63913fda4cb
Atsu <kamnknon@gmail.com>
2023-04-14 16:25:26 +0900

    First commit

---
+++ src/main/java/com/example/chinaornotbackend/ChinaOrNotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.chinaornotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinaOrNotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinaOrNotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chinaornotbackend/ChinaOrNotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chinaornotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinaOrNotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

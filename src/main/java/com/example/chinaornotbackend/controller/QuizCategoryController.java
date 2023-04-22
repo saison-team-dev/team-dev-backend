d00062449181959308245fc84acc851bb76f9f33
sosamjayo <marble0217@icloud.com>
2023-04-22 21:31:26 +0900

    add quizCategories data as json file

--- src/main/java/com/example/chinaornotbackend/controller/QuizCategoryController.java
+++ src/main/java/com/example/chinaornotbackend/controller/QuizCategoryController.java
@@ -4,13 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.chinaornotbackend.model.QuizCategory;
+
+
+
 @RestController
 @RequestMapping("api/quiz_categories")
 public class QuizCategoryController {
-        
-        @GetMapping("/")
-        private String getQuizCategories() {
-                String json = "json";
-                return json;
-        }
+  
+  @GetMapping("/")
+  private String getQuizCategory() {
+    return "bean";
+  }
 }

b8e5fc487542337d25f734a6fb0273f954448db6
sosamjayo <marble0217@icloud.com>
2023-04-23 13:27:06 +0900

    a

--- src/main/java/com/example/chinaornotbackend/controller/QuizCategoryController.java
+++ src/main/java/com/example/chinaornotbackend/controller/QuizCategoryController.java
@@ -4,16 +4,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.chinaornotbackend.model.QuizCategory;
+
+// import com.example.chinaornotbackend.model.QuizCategory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 
 @RestController
-@RequestMapping("api/quiz_categories")
+@RequestMapping("/api/quiz_categories")
 public class QuizCategoryController {
   
   @GetMapping("/")
-  private String getQuizCategory() {
-    return "bean";
+  private String getQuizCategory() throws JsonProcessingException, IOException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    JsonNode json = objectMapper.readTree(Paths.get("src/main/resources/data/quizCategories.json").toFile());
+    return json.toString();
   }
 }

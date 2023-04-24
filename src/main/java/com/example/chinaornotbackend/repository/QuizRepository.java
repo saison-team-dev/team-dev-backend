8f0e72801b14486466c33db0e6362a716f522311
Atsu <kamnknon@gmail.com>
2023-04-24 22:20:32 +0900

    update: quizをランダムに10件取得するようにQuizRepositoryとQuizServiceImplを修正

--- src/main/java/com/example/chinaornotbackend/repository/QuizRepository.java
+++ src/main/java/com/example/chinaornotbackend/repository/QuizRepository.java
@@ -3,7 +3,6 @@ package com.example.chinaornotbackend.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,6 +14,7 @@ public interface QuizRepository extends JpaRepository<Quiz, Long> {
 
   Optional<Quiz> findByQuestion(String question);
 
-  @Query("SELECT q FROM Quiz q JOIN FETCH q.answer")
-  List<Quiz> findAllWithAnswers(Pageable pageable);
+  // ランダムに10個のクイズを取得
+  @Query(value = "SELECT * FROM quizzes ORDER BY RANDOM() LIMIT 10;", nativeQuery = true)
+  List<Quiz> findAllWithAnswers();
 }

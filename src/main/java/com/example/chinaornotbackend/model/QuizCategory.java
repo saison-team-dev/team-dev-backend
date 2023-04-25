7db4f52dff10d481c02a6a90359679df56ac2e96
sosamjayo <marble0217@icloud.com>
2023-04-25 17:34:34 +0900

    feat: 昨日の追加
    dfsfhsdfh

--- src/main/java/com/example/chinaornotbackend/model/QuizCategory.java
+++ src/main/java/com/example/chinaornotbackend/model/QuizCategory.java
@@ -2,8 +2,6 @@ package com.example.chinaornotbackend.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -14,8 +12,6 @@ import lombok.Data;
 public class QuizCategory {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  // GenerationType.IDENTITYを指定すると、主キーの生成方法をデータベースに依存させることができる
   private Long id;
 
   @Column(nullable = false)
